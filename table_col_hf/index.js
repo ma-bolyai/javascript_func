2aeb7107b244c1f383984f30c08edfa913d40629
ma-bolyai <231857186+ma-bolyai@users.noreply.github.com>
2025-10-03 13:04:38 +0200

    js createElement

---
+++ table_col_hf/index.js
@@ -0,0 +1,48 @@
+/**
+ * @type {{theme: string, time: string, scientist1: string, scientist2:string}[]}
+ */
+const arr = [
+  {
+    theme: "Optika",
+    time: "XI. szazad",
+    scientist1: "Alhazen",
+  },
+  {
+    theme: "Asztronómia",
+    time: "reneszánsz",
+    scientist1: "Kepler",
+    scientist2: "Galilei",
+  },
+  {
+    theme: "Kvantumfizika",
+    time: "XIX-XX. század",
+    scientist1: "Max Planck",
+    scientist2: "Albert Einstein",
+  },
+  {
+    theme: "Modern fizika",
+    time: "XX-XXI. század",
+    scientist1: "Richard Feynman",
+    scientist2: "Stephen Hawking",
+  },
+];
+
+// const table = document.createElement("table");
+// const thead = document.createElement("thead");
+// const tbody = document.createElement("tbody");
+
+// let th = document.createElement("th");
+// let tr = document.createElement("tr");
+// let td = document.createElement("td");
+
+// document.body.appendChild(table);
+// table.appendChild(thead);
+// thead.appendChild(tr);
+
+// for (let i = 0; i < 3; i++) {
+//   tr.appendChild(th);
+//   th = document.createElement("th");
+// }
+
+// table.appendChild(tbody);
+// tbody.appendChild(tr);
