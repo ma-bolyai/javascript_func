898b2d452f2e32816cdf9bae8c6f75dc5b87eb36
ma-bolyai <231857186+ma-bolyai@users.noreply.github.com>
2025-11-14 13:06:43 +0100

    lot of work to do

---
+++ table_row/functions.js
@@ -0,0 +1,186 @@
+// TODO: finish
+
+/**
+ * @param {HTMLTableElement} table
+ * @param {string[]} array
+ */
+function renderTableHead(table, array) {
+  const thead = document.createElement("thead");
+  table.appendChild(thead);
+
+  const tr = document.createElement("tr");
+  thead.appendChild(tr);
+
+  for (const item of array) {
+    // const th = document.createElement("th");
+    // tr.appendChild(th);
+    // th.innerText = item;
+    createCell("th", item, tr);
+  }
+}
+
+/**
+ * @param {HTMLTableElement} table
+ * @param {CountryWriters[]} array
+ */
+function renderTableBody(table, array) {
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+  // problematic section begins
+
+  for (const item of array) {
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+
+    // const td_nat = document.createElement("td");
+    // tr.appendChild(td_nat);
+    const tdNat = createCell("td", item.nationality, tr);
+
+    tdNat.addEventListener("click", (e) => {
+      /**
+       * @type {HTMLTableCellElement}
+       */
+      const target = e.target;
+      const isMarked = target.parentElement.querySelector(".marked");
+      if (!isMarked) {
+        target.classList.add("marked");
+      } else {
+        target.classList.remove("marked");
+      }
+    });
+
+    // const td_name = document.createElement("td");
+    // tr.appendChild(td_name);
+    const tdName = createCell("td", item.name, tr);
+
+    // const td_title = document.createElement("td");
+    // tr.appendChild(td_title);
+    const tdTitle = createCell("td", item.title, tr);
+
+    // td_nat.innerText = item.nationality;
+    // td_name.innerText = item.name;
+    // td_title.innerText = item.title;
+
+    // fixed a typo and it works, im an idiot
+
+    if (item.name && item.title2) {
+      const tr = document.createElement("tr");
+      tbody.appendChild(tr);
+
+      //   const td_name2 = document.createElement("td");
+      //   tr.appendChild(td_name2);
+      const tdName2 = createCell("td", item.name2, tr);
+
+      //   const td_title2 = document.createElement("td");
+      //   tr.appendChild(td_title2);
+      const tdTitle2 = createCell("td", item.title2, tr);
+
+      tdNat.rowSpan = 2;
+    }
+  }
+}
+
+/**
+ * @param {HTMLTableElement} table
+ * @param {CountryWriters[]} array
+ */
+function renderTableRow(table, array) {
+  const target = table.querySelector("tbody");
+  for (const item of array) {
+    for (const key in item) {
+    }
+  }
+}
+
+/**
+ * @param {string} cellType either th or td
+ * @param {string} cellContent desired content of the cell
+ * @param {HTMLTableRowElement} parentRow specific row of the table
+ * @returns {HTMLTableCellElement}
+ */
+
+function createCell(cellType, cellContent, parentRow) {
+  const cell = document.createElement(cellType);
+  cell.innerText = cellContent;
+  parentRow.appendChild(cell);
+
+  return cell;
+}
+
+/**
+ * @param {Event} e
+ */
+function htmlEventListener(e) {
+  /**
+   * @type {HTMLFormElement}
+   */
+  const form = document.getElementById("htmlform");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    /**
+     * @type {HTMLFormElement}
+     */
+    const target = e.target;
+
+    /**
+     * @type {{nationality: HTMLInputElement, name: HTMLInputElement, name2: HTMLInputElement, title: HTMLInputElement, title2: HTMLInputElement}}
+     */
+    const inputs = {
+      nationality: target.querySelector("#nemzetiseg"),
+      name: target.querySelector("#szerzo1"),
+      name2: target.querySelector("#szerzo2"),
+      title: target.querySelector("#mu1"),
+      title2: target.querySelector("#mu2"),
+    };
+
+    const values = {
+      nationality: inputs.nationality.value,
+      name: inputs.name.value,
+      name2: inputs.name2.value,
+      title: inputs.title.value,
+      title2: inputs.title2.value,
+    };
+
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+
+    // const td_nat = document.createElement("td");
+    // tr.appendChild(td_nat);
+    const tdNat = createCell("td", values.nationality, tr);
+
+    tdNat.addEventListener("click", (e) => {
+      /**
+       * @type {HTMLTableCellElement}
+       */
+      const target = e.target;
+      target.classList.add("marked");
+    });
+
+    // const td_name = document.createElement("td");
+    // tr.appendChild(td_name);
+    const tdName = createCell("td", values.name, tr);
+
+    // const td_title = document.createElement("td");
+    // tr.appendChild(td_title);
+    const tdTitle = createCell("td", values.title, tr);
+
+    // shitty rowspan cells
+
+    if (values.name && values.title2) {
+      const tr = document.createElement("tr");
+      tbody.appendChild(tr);
+
+      //   const td_name2 = document.createElement("td");
+      //   tr.appendChild(td_name2);
+      const tdName2 = createCell("td", values.name2);
+
+      //   const td_title2 = document.createElement("td");
+      //   tr.appendChild(td_title2);
+      const tdTitle2 = createCell("td", values.title2);
+
+      tdNat.rowSpan = 2;
+    }
+  });
+}
--- table_row/index.js
+++ table_row/index.js
@@ -1,5 +1,9 @@
 /**
- *  @type {{nationality: string, name: string, title: string, name2: string, title2: string}[]}
+ * @typedef {{nationality: string, name: string, title: string, name2: string, title2: string}} CountryWriters
+ */
+
+/**
+ *  @type {CountryWriters[]}
  */
 const arr = [
   {
@@ -31,174 +35,13 @@ const arr = [
 /**
  * @type {{headers: string}[]}
  */
-const headers = ["Nemzetiseg", "Szerző", "Mű"];
+const headers = ["Nemzetiség", "Szerző", "Mű"];
 
-const generatedTable = renderTableBody(arr);
-document.body.appendChild(generatedTable);
+const table = document.createElement("table");
+document.body.appendChild(table);
 
-populateTableBody(generatedTable, headers, arr);
+renderTableHead(table, headers);
 
-// TODO: finish
+renderTableBody(table, arr);
 
-/**
- * @returns {HTMLTableElement}
- * @param {object[]} array
- */
-function renderTableBody(array) {
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tbody = document.createElement("tbody");
-
-  table.appendChild(thead);
-  table.appendChild(tbody);
-
-  const tr = document.createElement("tr");
-  thead.appendChild(tr);
-
-  for (const item of headers) {
-    const th = document.createElement("th");
-    tr.appendChild(th);
-  }
-
-  // problematic section begins
-
-  for (const item of array) {
-    const tr = document.createElement("tr");
-    tbody.appendChild(tr);
-
-    const td_nat = document.createElement("td");
-    tr.appendChild(td_nat);
-
-    td_nat.addEventListener("click", (e) => {
-      /**
-       * @type {HTMLTableCellElement}
-       */
-      const target = e.target;
-      target.classList.add("marked");
-    });
-
-    const td_name = document.createElement("td");
-    tr.appendChild(td_name);
-
-    const td_title = document.createElement("td");
-    tr.appendChild(td_title);
-
-    td_nat.innerText = item.nationality;
-    td_name.innerText = item.name;
-    td_title.innerText = item.title;
-
-    // fixed a typo and it works, im an idiot
-
-    if (item.name && item.title2) {
-      const tr = document.createElement("tr");
-      tbody.appendChild(tr);
-
-      const td_name2 = document.createElement("td");
-      tr.appendChild(td_name2);
-
-      const td_title2 = document.createElement("td");
-      tr.appendChild(td_title2);
-
-      td_name2.innerText = item.name2;
-      td_title2.innerText = item.title2;
-
-      td_nat.rowSpan = 2;
-    }
-  }
-
-  // user input section
-
-  /**
-   * @type {HTMLFormElement}
-   */
-  const form = document.getElementById("htmlform");
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    /**
-     * @type {HTMLFormElement}
-     */
-    const target = e.target;
-
-    /**
-     * @type {{nationality: HTMLInputElement, name: HTMLInputElement, name2: HTMLInputElement, title: HTMLInputElement, title2: HTMLInputElement}}
-     */
-    const inputs = {
-      nationality: target.querySelector("#nemzetiseg"),
-      name: target.querySelector("#szerzo1"),
-      name2: target.querySelector("#szerzo2"),
-      title: target.querySelector("#mu1"),
-      title2: target.querySelector("#mu2"),
-    };
-
-    const values = {
-      nationality: inputs.nationality.value,
-      name: inputs.name.value,
-      name2: inputs.name2.value,
-      title: inputs.title.value,
-      title2: inputs.title2.value,
-    };
-
-    const tr = document.createElement("tr");
-    tbody.appendChild(tr);
-
-    const td_nat = document.createElement("td");
-    tr.appendChild(td_nat);
-
-    td_nat.addEventListener("click", (e) => {
-      /**
-       * @type {HTMLTableCellElement}
-       */
-      const target = e.target;
-      target.classList.add("marked");
-    });
-
-    const td_name = document.createElement("td");
-    tr.appendChild(td_name);
-
-    const td_title = document.createElement("td");
-    tr.appendChild(td_title);
-
-    td_nat.innerText = values.nationality;
-    td_name.innerText = values.name;
-    td_title.innerText = values.title;
-
-    // shitty rowspan cells
-
-    if (values.name && values.title2) {
-      const tr = document.createElement("tr");
-      tbody.appendChild(tr);
-
-      const td_name2 = document.createElement("td");
-      tr.appendChild(td_name2);
-
-      const td_title2 = document.createElement("td");
-      tr.appendChild(td_title2);
-
-      td_name2.innerText = values.name2;
-      td_title2.innerText = values.title2;
-
-      td_nat.rowSpan = 2;
-    }
-  });
-
-  return table;
-}
-
-/**
- * @returns {void}
- * @param {HTMLTableElement} table
- * @param {string[]} array1
- * @param {object[]} array2
- */
-function populateTableBody(table, array1, array2) {
-  const thEmpty = table.getElementsByTagName("th");
-
-  let i = 0;
-  for (const item of array1) {
-    thEmpty[i].innerText = item;
-    i++;
-  }
-
-  const tdEmpty = table.getElementsByTagName("td");
-}
+renderTableRow(table, arr);
